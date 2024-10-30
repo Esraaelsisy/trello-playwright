6df89192ed102a1e511c027efe43badc0f470f6f
Esraaelsisy <esraa.elsisy@gmail.com>
2024-10-30 11:27:16 +0100

    Update boardTests to use the data from test-data.json file

--- tests/BoradTests.spec.ts
+++ tests/BoradTests.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from "../fixtures/test.setup";
+import testData from "../data/test-data.json";
 
 test("Create a New Trello Board", async ({ pm, page }) => {
-  await test.step('Create a new board named "Bob\'s Pool Maintenance"', async () => {
-    await pm.homePage.createNewBoard("Bob's Pool Maintenance");
+  await test.step(`Create a new board named "${testData.board.name}"`, async () => {
+    await pm.homePage.createNewBoard(testData.board.name);
   });
 
   await test.step("Verify the new board is created with the correct name", async () => {
-    await expect(pm.boardPage.boardTitle).toHaveText("Bob's Pool Maintenance");
+    await expect(pm.boardPage.boardTitle).toHaveText(testData.board.name);
   });
 });
