4eab668ad452ba40ea7b171ec7a5ff7e3de37481
Esraaelsisy <esraa.elsisy@gmail.com>
2024-10-30 11:27:02 +0100

    Update listTests to use the data from test-data.json file

--- tests/ListTests.spec.ts
+++ tests/ListTests.spec.ts
@@ -1,45 +1,52 @@
 import { test, expect } from "../fixtures/test.setup";
+import testData from "../data/test-data.json";
 
 test("Create a New List on Board", async ({ pm }) => {
   await test.step("Create a new board", async () => {
-    await pm.homePage.createNewBoard("Bob's Pool Maintenance");
+    await pm.homePage.createNewBoard(testData.board.name);
   });
 
   await test.step("Verify the new board is created with the correct name", async () => {
-    await expect(pm.boardPage.boardTitle).toHaveText("Bob's Pool Maintenance");
+    await expect(pm.boardPage.boardTitle).toHaveText(testData.board.name);
   });
 
-  await test.step('Add a list named "Prospects" to the board', async () => {
-    await pm.boardPage.addList("Prospects");
+  await test.step(`Add a list named "${testData.lists[0].name}" to the board`, async () => {
+    await pm.boardPage.addList(testData.lists[0].name);
   });
 
-  await test.step('Verify the list named "Prospects" is added to the board', async () => {
-    await expect(pm.boardPage.listTitle.last()).toContainText("Prospects");
+  await test.step(`Verify the list named "${testData.lists[0].name}" is added to the board`, async () => {
+    await expect(pm.boardPage.listTitle.last()).toContainText(
+      testData.lists[0].name
+    );
   });
 });
 
 test("Create Another List on a Board", async ({ pm }) => {
   await test.step("Create a new board", async () => {
-    await pm.homePage.createNewBoard("Bob's Pool Maintenance");
+    await pm.homePage.createNewBoard(testData.board.name);
   });
 
   await test.step("Verify the new board is created with the correct name", async () => {
-    await expect(pm.boardPage.boardTitle).toHaveText("Bob's Pool Maintenance");
+    await expect(pm.boardPage.boardTitle).toHaveText(testData.board.name);
   });
 
-  await test.step('Add a list named "Prospects" to the board', async () => {
-    await pm.boardPage.addList("Prospects");
+  await test.step(`Add a list named "${testData.lists[0].name}" to the board`, async () => {
+    await pm.boardPage.addList(testData.lists[0].name);
   });
 
-  await test.step('Verify the list named "Prospects" is added to the board', async () => {
-    await expect(pm.boardPage.listTitle.last()).toContainText("Prospects");
+  await test.step(`Verify the list named "${testData.lists[0].name}" is added to the board`, async () => {
+    await expect(pm.boardPage.listTitle.last()).toContainText(
+      testData.lists[0].name
+    );
   });
 
-  await test.step('Add a list named "In Progress" to the board', async () => {
-    await pm.boardPage.addList("In Progress");
+  await test.step(`Add a list named "${testData.lists[1].name}" to the board`, async () => {
+    await pm.boardPage.addList(testData.lists[1].name);
   });
 
-  await test.step('Verify the list named "Prospects" is added to the board', async () => {
-    await expect(pm.boardPage.listTitle.last()).toContainText("In Progress");
+  await test.step(`Verify the list named "${testData.lists[1].name}" is added to the board`, async () => {
+    await expect(pm.boardPage.listTitle.last()).toContainText(
+      testData.lists[1].name
+    );
   });
 });
